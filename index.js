192a3bfbd9b539e25fb5957956db35c645201b08
Bsc-40-20 <bsc-40-20@unima.ac.mw>
2023-04-07 11:47:06 +0200

    post route

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express')
+const express = require("express");
 
 // getting the post object
-const Post = require("./src/Posts/Posts.model")
+const Post = require("./src/Posts/Posts.model");
 
 // creating an instance of express
-const app = express()
+const app = express();
 
 // getting a post
-app.get('/', (request , response) => {
-    return response.json(request)
-})
+app.get("/", (request, response) => {
+  return response.json(request.headers);
+});
+
+// creating a post
+app.post("/api/v1/post", (request, response) => {
+  return response.json({ ...Post, id: "somerandomID" });
+});
 
 // server port
-// 
+//
 
 // creating a server
-app.listen(3000, () => console.log('Server Running'))
+app.listen(3000, () => console.log("Server Running"));
